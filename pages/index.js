71e1659376cc77aa091a9595e0af28556c738361
jiro-farah-cko <76951754+jiro-farah-cko@users.noreply.github.com>
2021-10-13 14:32:53 +0400

    Move views to pages folder, add comments

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,34 @@
-export { default, getStaticProps } from "../views/home";
+import { getSingleTypeData } from "../utils/prismic";
+import { RichText } from "prismic-reactjs";
+import useUpdatePreviewRef from "../utils/useUpdatePreviewRef";
+
+export default function Home({ data, previewRef, id }) {
+  useUpdatePreviewRef(previewRef, id);
+  if (data) {
+    return <>{RichText.asText(data.title)}</>;
+  }
+  return null;
+}
+
+export async function getStaticProps(context) {
+  const { previewData, locale } = context;
+  const previewRef = previewData ? previewData.ref : null;
+  const homepageData = await getSingleTypeData("home", previewRef, locale);
+
+  if (!homepageData) {
+    context.statusCode = 404;
+    return {
+      notFound: true,
+      revalidate: 60,
+    };
+  }
+
+  return {
+    props: {
+      data: homepageData?.data || null,
+      previewRef,
+      id: homepageData?.id || null,
+      revalidate: 1,
+    },
+  };
+}
